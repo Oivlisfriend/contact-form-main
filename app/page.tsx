1351f258f46a5c3a0891e46a5a5f9317a695c087
Oivlisfriend <robertomarcelogongo@gmail.com>
2024-09-05 09:11:53 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,48 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-emerald-50 h-screen flex justify-center items-center">
+      <form className="flex flex-col gap-5 w-80 lg:w-[36rem] xl:w-[36rem] 2xl:w-[36rem] px-6 py-8 rounded-md bg-white">
+        <h1 className="font-bold text-xl">Contact Us</h1>
+        <div className="flex gap-3 flex-col lg:flex-row xl:flex-row 2xl:flex">
+          <div className="flex flex-1 lg:w-1/2 xl:w-1/2 2xl:w-1/2 flex-col gap-1.5">
+            <p className="text-sm">First Name <span className="text-emerald-700">*</span></p>
+            <input className="flex-1 border-2 outline-none ps-1 rounded-md py-1" name="name" id="name" required />
+          </div>
+          <div className="flex flex-col flex-1 lg:w-1/2 xl:w-1/2 2xl:w-1/2 gap-1.5">
+            <p className="text-sm">Last Name <span className="text-emerald-700">*</span></p>
+            <input className="flex-1 border-2 outline-none ps-1 rounded-md py-1" name="name" id="name" required />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex flex-col gap-1.5">
+          <p className="text-sm">Email Address <span className="text-emerald-700">*</span></p>
+          <input className="flex-1 border-2 outline-none ps-1 rounded-md py-1" name="name" id="name" required />
+        </div>
+        <div className="flex flex-col gap-1.5">
+          <p className="text-sm">Query Type <span className="text-emerald-700">*</span></p>
+          <div className="flex gap-4 lg:gap-2 xl:gap-2 2xl:gap-2 flex-col lg:flex-row xl:flex-row 2xl:flex-row">
+            <div className="flex gap-1 flex-1 lg:w-1/2 xl:w-1/2 2xl:w-1/2 border-2 rounded-md px-4 py-1">
+              <input type="radio" className="border-2 outline-none ps-1 rounded-md py-1" name="name" id="name" required />
+              <span>General Enquiry</span>
+            </div>
+            <div className="flex gap-1 flex-1 lg:w-1/2 xl:w-1/2 2xl:w-1/2 border-2 rounded-md px-4 py-1">
+              <input type="radio" className="border-2 outline-none ps-1 rounded-md py-1" name="name" id="name" required />
+              <span>Support Request</span>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-1.5">
+          <p className="text-sm">Message <span className="text-emerald-700">*</span></p>
+          <textarea rows={3.5} className="flex-1 border-2 outline-none ps-1 rounded-md py-1" name="name" id="name" required />
+        </div>
+        <div className="flex gap-3.5 items-center">
+          <input type="checkbox" name="" id="" required />
+          <span className="text-sm">I consent to being contacted by the team</span>
+        </div>
+        <div className="bg-emerald-700 text-sm py-2.5 text-center font-medium rounded-lg text-white">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
